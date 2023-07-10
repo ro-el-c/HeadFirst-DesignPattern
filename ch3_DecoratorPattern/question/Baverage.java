2c297aabd28a994db03062bee8dedbcb13681d4c
ro-el <alcxzp@gmail.com>
2023-07-10 18:26:01 +0900

    fix: Decorator Pattern 커피 주문 시스템 임시방편의 문제 해결 2

--- ch3_DecoratorPattern/question/Baverage.java
+++ ch3_DecoratorPattern/question/Baverage.java
@@ -2,8 +2,45 @@ package question;
 
 public abstract class Baverage {
     String description = "Unknown Beverage";
+    Boolean milk = false;
+    Boolean soy = false;
+    Boolean mocha = false;
+    Boolean whip = false;
+
     public abstract double cost();
     public String getDescription() {
         return description;
+    }
+
+    public Boolean hasMilk() {
+        return milk;
+    }
+
+    public void setMilk(Boolean milk) {
+        this.milk = milk;
+    }
+
+    public Boolean hasSoy() {
+        return soy;
+    }
+
+    public void setSoy(Boolean soy) {
+        this.soy = soy;
+    }
+
+    public Boolean hasMocha() {
+        return mocha;
+    }
+
+    public void setMocha(Boolean mocha) {
+        this.mocha = mocha;
+    }
+
+    public Boolean hasWhip() {
+        return whip;
+    }
+
+    public void setWhip(Boolean whip) {
+        this.whip = whip;
     }
 }
